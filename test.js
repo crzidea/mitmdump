ddcc91cb4892f6701ebf73b1ed41ce1bafbfa379
Crzidea <i@crzidea.com>
2017-10-07 11:52:38 +0800

    Check if content is valuable

--- test.js
+++ test.js
@@ -32,11 +32,49 @@ describe('mitmdump', () => {
     })
   })
   describe('http2https proxy', () => {
-    it('should rewrite and record request', async () => {
-      const response = await $got('http://crzidea.com?test')
+    it('should rewrite and record request, html', async () => {
+      const response = await $got('http://github.com?test')
+      assert.equal(response.statusCode, 200)
+      assert(response.headers)
+      assert(response.headers['x-mitm-valuable'])
+      assert(response.body)
+    })
+    it('should rewrite and record request, javascript', async () => {
+      const response = await $got('http://crzidea.com/js/main.js')
+      assert.equal(response.statusCode, 200)
+      assert(response.headers)
+      assert(response.headers['x-mitm-valuable'])
+      assert(response.body)
+    })
+    it('should rewrite and record request, json', async () => {
+      const response = await $got(
+        'http://api.github.com/_private/browser/stats',
+        {
+          json: true,
+          body: {}
+        }
+      )
+      assert.equal(response.statusCode, 200)
+      assert(response.headers)
+      assert(response.headers['x-mitm-valuable'])
+      assert(response.body)
+    })
+    it('should rewrite and record request, image', async () => {
+      const response = await $got('http://assets-cdn.github.com/images/modules/site/universe-logo.png')
       assert.equal(response.statusCode, 200)
       assert(response.headers)
       assert(response.body)
+    })
+    it('should not rewrite, blob', async () => {
+      let response = await $got(
+        'http://api.github.com/_private/browser/stats',
+        {
+          method: 'POST',
+          body: new Buffer(10)
+        }
+      )
+      assert.equal(response.statusCode, 301)
+      assert(response.headers.location)
     })
   })
 })
